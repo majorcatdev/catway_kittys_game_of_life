bf81f6f09a99e48d9ddc319a30e33481e3ee7666
majorcatdev <120533675+majorcatdev@users.noreply.github.com>
2023-05-25 20:16:28 -0700

    development started

--- conway_twittys_game_of_life.py
+++ conway_twittys_game_of_life.py
@@ -5,11 +5,19 @@ from random import randint
 
 
 root = tk.Tk()
-WIDTH=840
-HEIGHT=680
-TILESIZE=16
+WIDTH=800
+HEIGHT=600
+TILESIZE=32
 keysDown={}
 RUNNING=True
+AWAKECAT = tk.PhotoImage(file='cat1.png')
+ASLEEPCAT= tk.PhotoImage(file='cat2.png')
+
+
+def randbool():
+    if(randint(0,1)==1):
+        return True
+    return False
 
 
 root.geometry(str(WIDTH)+'x'+str(HEIGHT))
@@ -17,7 +25,7 @@ root.title('conway twitty\'s game of life')
 
 canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg='white')
 canvas.pack(anchor=tk.CENTER, expand=True)
-
+ 
 
 #widget.bind(event, handler, add=None)
 
@@ -36,6 +44,35 @@ root.bind("<KeyPress>",keyDown)
 root.bind("<KeyRelease>",keyUp)
 root.protocol("WM_DELETE_WINDOW", window_exit)
 
+class Tile:
+    def __init__(self,x,y,size):
+        self.x=x
+        self.y=y
+        self.size=size
+        self.awake=False
+    def draw(self):
+        if(self.awake):
+            canvas.create_image((self.x*self.size+self.size/2, self.y*self.size+self.size/2+1),image=AWAKECAT)
+        else:
+            canvas.create_image((self.x*self.size+self.size/2, self.y*self.size+self.size/2+1),image=ASLEEPCAT)
+    
+def makeGrid():
+    grid=[]
+    for y in range((HEIGHT//TILESIZE)):
+        temp=[]
+        for x in range((WIDTH//TILESIZE)):
+            temp.append(Tile(x,y,TILESIZE))
+        grid.append(temp)
+    return grid
+grid=makeGrid()
+
+def render(grid):
+    canvas.create_rectangle((0, 0), (WIDTH, HEIGHT), fill='white')
+    for y in grid:
+        for x in y:
+            x.draw()
+
+render(grid)
 
 
 while RUNNING:

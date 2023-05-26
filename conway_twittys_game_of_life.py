acbc33ed2686e6d3974cd13756f2b8f8b9e326c5
majorcatdev <120533675+majorcatdev@users.noreply.github.com>
2023-05-25 17:25:33 -0700

    Initial commit

---
+++ conway_twittys_game_of_life.py
@@ -0,0 +1,39 @@
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+
+root = tk.Tk()
+WIDTH=800
+HEIGHT=600
+keysDown={}
+
+
+
+root.geometry(str(WIDTH)+'x'+str(HEIGHT))
+root.title('conway twitty\'s game of life')
+
+canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg='white')
+canvas.pack(anchor=tk.CENTER, expand=True)
+
+
+#widget.bind(event, handler, add=None)
+
+def keyDown(event):
+    keysDown[event]=True
+
+def keyUp(event):
+    keysDown[event]=False
+
+
+
+root.bind("<KeyPress>",keyDown)
+root.bind("<KeyRelease>",keyUp)
+
+
+
+while True:
+    #put main loop here
+
+
+    root.update_idletasks()
+    root.update()
